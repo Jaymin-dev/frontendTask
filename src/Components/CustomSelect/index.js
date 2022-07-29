5177606341d12f06ba1dd54df4f487bfff0a0fd8
JayminPatel <jayminpatel4998@gmail.com>
2022-07-29 16:11:18 +0530

    fix:add search and dropdown

---
+++ src/Components/CustomSelect/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-useless-computed-key */
+import * as React from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import cs from "classnames";
+import "./styles.scss";
+
+const CustomSelect = ({
+  size = "small",
+  options = [],
+  placeholder,
+  label,
+  widthBorder = true,
+  displayEmpty = true,
+  ...res
+}) => {
+  return (
+    <>
+      <FormControl
+        sx={{ m: 1, minWidth: "100%", maxWidth: 300,marginLeft:0,margin:0.2 }}
+        // className={cs("customSelectWrapper", { ["noBorder"]: !widthBorder })}
+      >
+        {!displayEmpty && label && <InputLabel >{label}</InputLabel>}
+        <Select
+          displayEmpty={displayEmpty}
+          inputProps={{ "aria-label": "Without label" }}
+          size={size}
+          variant="outlined"
+          classes={{ root: "testing123" }}
+          {...res}
+        >
+          {placeholder && (
+            <MenuItem disabled value="">
+              {placeholder}
+            </MenuItem>
+          )}
+
+          {options.map((o) => (
+            <MenuItem value={o.value}>{o.lable}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+};
+export default CustomSelect;
--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -8,6 +8,8 @@ import { alpha, styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
+import { Button, FormHelperText, Grid, InputAdornment, OutlinedInput } from "@mui/material";
+import CustomSelect from "../../Components/CustomSelect";
 
 const columns = [
   { field: "logId", headerName: "Log ID", width: 150 },
@@ -40,51 +42,11 @@ const columns = [
     sortable: false,
     width: 230,
     valueGetter: (params) =>
-      `${params.row.creationTimestamp.split(" ")[0]} / ${
-        params.row.creationTimestamp.split(" ")[1]
+      `${params.row.creationTimestamp.split(" ")[0]} / ${params.row.creationTimestamp.split(" ")[1]
       }`,
   },
 ];
 
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  MuiInputLabel: {
-    fontSize: 16,
-  },
-  "label + &": {
-    marginTop: theme.spacing(3),
-  },
-  "& .MuiInputBase-input": {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
-    border: "1px solid #ced4da",
-    fontSize: 16,
-    width: "auto",
-    padding: "10px 12px",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}));
 
 export const Dashboard = () => {
   const {
@@ -96,12 +58,123 @@ export const Dashboard = () => {
   }, []);
   return (
     <Container>
-      <FormControl variant="standard">
-        <InputLabel shrink htmlFor="bootstrap-input">
-          Bootstrap
-        </InputLabel>
-        <BootstrapInput defaultValue="react-bootstrap" id="bootstrap-input" />
-      </FormControl>
+      <Grid container spacing={1}>
+        <Grid item xs>
+          <FormControl sx={{ m: 1, width: '18ch' }} variant="outlined">
+            <FormHelperText id="outlined-weight-helper-text"
+              sm={{ mb: 2 }} style={{ color: "#000",marginLeft:0}}
+            >Employee Name</FormHelperText>
+            <OutlinedInput
+              placeholder="e.g.Admin.User"
+              id="outlined-adornment-weight"
+              // value={values.weight}
+              // onChange={handleChange('weight')}
+              aria-describedby="outlined-weight-helper-text"
+              style={{
+                height: 30,
+              }}
+            />
+
+          </FormControl>
+        </Grid>
+        <Grid item xs sx={{ m: 1, width: '18ch',marginTop:1.5 }}>
+          <InputLabel sm={{ mb: 2 }} style={{ color: "#000" ,fontSize:12,}}>
+            Action Type
+          </InputLabel>
+          <CustomSelect
+            options={[
+              { value: "ASAP", lable: "ASAP" },
+              { value: "Within 1 Months", lable: "Within 1 Months" },
+              { value: "Within 6 Months", lable: "Within 6 Months" },
+              { value: "Within 12 Months", lable: "Within 12 Months" },
+            ]}
+            name="rating"
+            // onChange={handleChange}
+            // value={data.rating}
+            placeholder="SELECT"
+            style={{
+              height: 30
+            }}
+          />
+        </Grid>
+        <Grid item xs sx={{ m: 1, width: '18ch',marginTop:1.5 }}>
+        <InputLabel sm={{ mb: 2 }} style={{ color: "#000" ,fontSize:12}}>
+            Application Type
+          </InputLabel>
+          <CustomSelect
+            options={[
+              { value: "ASAP", lable: "ASAP" },
+              { value: "Within 1 Months", lable: "Within 1 Months" },
+              { value: "Within 6 Months", lable: "Within 6 Months" },
+              { value: "Within 12 Months", lable: "Within 12 Months" },
+            ]}
+            name="rating"
+            // onChange={handleChange}
+            // value={data.rating}
+            placeholder="SELECT"
+            style={{
+              height: 30
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <FormControl sx={{ m: 1, width: '18ch' }} variant="outlined">
+            <FormHelperText id="outlined-weight-helper-text"
+              sm={{ mb: 2 }} style={{ color: "#000" ,marginLeft:0}}
+            >From Date</FormHelperText>
+            <OutlinedInput
+              placeholder="Select Date"
+              id="outlined-adornment-weight"
+              // value={values.weight}
+              // onChange={handleChange('weight')}
+              aria-describedby="outlined-weight-helper-text"
+              style={{
+                height: 30
+              }}
+            />
+
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+          <FormControl sx={{ m: 1, width: '18ch' }} variant="outlined">
+            <FormHelperText id="outlined-weight-helper-text"
+              sm={{ mb: 2 }} style={{ color: "#000",marginLeft:0 }}
+            >To Date</FormHelperText>
+            <OutlinedInput
+              placeholder="Select Date"
+              id="outlined-adornment-weight"
+              // value={values.weight}
+              // onChange={handleChange('weight')}
+              aria-describedby="outlined-weight-helper-text"
+              style={{
+                height: 30
+              }}
+            />
+
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+          <FormControl sx={{ m: 1, width: '18ch' }} variant="outlined">
+            <FormHelperText id="outlined-weight-helper-text"
+              sm={{ mb: 2 }} style={{ color: "#000",marginLeft:0 }}
+            >Application ID</FormHelperText>
+            <OutlinedInput
+              placeholder="e.g.219841/2021"
+              id="outlined-adornment-weight"
+              // value={values.weight}
+              // onChange={handleChange('weight')}
+              aria-describedby="outlined-weight-helper-text"
+              style={{
+                height: 30
+              }}
+            />
+
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+        <Button variant="contained" sx={{ m: 1, width: '18ch' }} style={{marginTop:30,textTransform:'capitalize',height:30}}>Search Logger</Button>
+        </Grid>
+      </Grid>
       <Box sx={{ height: 650, width: "100%", boxShadow: 3 }}>
         <DataGrid
           getRowId={(i) => i.logId}
